7c17a90fb141952d8e758a7bddd5714487447eeb
Leonardo Ferreira <46528149+LeoFodon@users.noreply.github.com>
2019-11-03 18:49:27 -0300

    app-routing
    Veja se adicionando esse authguard funfa?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,15 +9,21 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+      path: 'cadastro-usuario',
+    // tslint:disable-next-line:max-line-length
+    loadChildren: () => import('./cadastro-usuario/cadastro-usuario.module').then(m => m.CadastroUsuarioPageModule), canActivate : [AuthGuardService]},
   },
-  { path: 'grupos-cadastro', loadChildren: './grupos-cadastro/grupos-cadastro.module#GruposCadastroPageModule',},
+  { path: 'grupos-cadastro', loadChildren: './grupos-cadastro/grupos-cadastro.module#GruposCadastroPageModule', },
   { path: 'grupos-detalhes', loadChildren: './grupos-detalhes/grupos-detalhes.module#GruposDetalhesPageModule', },
   { path: 'grupos-lista', loadChildren: './grupos-lista/grupos-lista.module#GruposListaPageModule', },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'cadastro-usuario', loadChildren: './cadastro-usuario/cadastro-usuario.module#CadastroUsuarioPageModule', },
-  { path: 'logoff', loadChildren: './logoff/logoff.module#LogoffPageModule',  },  { path: 'contato', loadChildren: './contato/contato.module#ContatoPageModule' },
+  { path: 'logoff', loadChildren: './logoff/logoff.module#LogoffPageModule',  },
+  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule', canActivate : [AuthGuardService]},
+  { path: 'contato', loadChildren: './contato/contato.module#ContatoPageModule' },
+  { path: 'sobre', loadChildren: './sobre/sobre.module#SobrePageModule' },
+  // tslint:disable-next-line:max-line-length
+  { path: 'politica-de-privacidade', loadChildren: './politica-de-privacidade/politica-de-privacidade.module#PoliticaDePrivacidadePageModule' },
 
 
 
